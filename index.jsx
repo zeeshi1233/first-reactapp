5ff8fc9f9e2730b7d653845eba743f412d5d9734
Zeeshan Ahmed <114758827+zeeshi1233@users.noreply.github.com>
2023-08-15 19:10:44 +0500

    Add files via upload

---
+++ index.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+<>
+<div className='container' >
+<nav>
+<a href=""><img src="https://mobirise.com/extensions/resultm5/affiliate-marketing/assets/images/logo.png" alt="" /> </a>
+<ul className='navbar'>
+    <li className='nav-item'><a href="">Home</a></li>
+    <li className='nav-item'><a href="">About</a></li>
+    <li className='nav-item'><a href="">Contact</a></li>
+    <li className='nav-item'><a href="">Marketing</a></li>
+    <li className='nav-item'><a href="">Faqs</a></li>
+</ul>
+<div className="nav-end">
+    <button>Login</button>
+    <button className='signup' >SignUp</button>
+</div>
+</nav>
+ 
+</div>
+
+
+<div id="hero">
+
+    <div id="content">
+<h3>Afficiliat Marketing</h3>
+    <p>From the architect's words and pain, and it will come to pass, he hastened to flee, but no one knows the troubles we have caused by rejecting our duties and rejecting them at a similar time!</p>
+    <button>Start Now</button>
+    </div>
+
+    <div id="hero-img">
+        <img height={'300px'} width={'450px'}  src="https://mobirise.com/extensions/resultm5/affiliate-marketing/assets/images/image4.jpg" alt="" srcset="" />
+<img width={'300px'} height={'400px'} id='img' src="https://mobirise.com/extensions/resultm5/affiliate-marketing/assets/images/image16.jpg" alt="" />
+
+</div>
+
+</div>
+</>
+
+
+);
+
